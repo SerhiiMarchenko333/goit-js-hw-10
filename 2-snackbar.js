6b6f639ed52b57be8033cc5bf00072a257b87d5b
SerhiiMarchenko333 <msv2@ukr.net>
2025-02-07 07:51:29 +0000

    Deploying to gh-pages from @ SerhiiMarchenko333/goit-js-hw-10@e35404893756903c9ed1106ca8658cfb9e2121e9 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as r}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",function(t){t.preventDefault();const s=parseInt(t.target.delay.value),i=t.target.state.value;new Promise((e,o)=>{setTimeout(()=>{i==="fulfilled"?e(s):o(s)},s)}).then(e=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
